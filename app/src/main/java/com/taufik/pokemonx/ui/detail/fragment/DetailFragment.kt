af14567c43ed745508d6baa71b9d38c55fd1ab57
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-10-21 08:14:46 +0700

    Add code to handle detail info.

--- app/src/main/java/com/taufik/pokemonx/ui/detail/fragment/DetailFragment.kt
+++ app/src/main/java/com/taufik/pokemonx/ui/detail/fragment/DetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.taufik.pokemonx.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +14,8 @@ class DetailFragment : Fragment() {
 
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
+
+    private var name = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,10 +28,24 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        getBundleData()
+        showPokemonInfo()
+    }
+
+    private fun getBundleData() {
+        name = arguments?.getString(EXTRA_DETAIL).orEmpty()
+    }
+
+    private fun showPokemonInfo() {
+        Toast.makeText(requireContext(), name, Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        const val EXTRA_DETAIL = "com.taufik.pokemonx.ui.detail.fragment.EXTRA_DETAIL"
     }
 }
